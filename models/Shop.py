51c7b47731eefc67a47ca5fb71cd54c19e345cd7
mhduusop <mhduusop@gmail.com>
2022-12-28 16:55:23 +0700

    add some code in file Shop :D

--- models/Shop.py
+++ models/Shop.py
@@ -1,2 +1,13 @@
+import uuid
+
+
 class Shop:
-    pass
+    locationId: int = 0
+    productId: int = 0
+    name: str = ''
+
+    def __init__(self, locationId: int, productId: int, name: str) -> None:
+        self._id = str(uuid.uuid4())
+        self.locationId = locationId
+        self.productId = productId
+        self.name = name
